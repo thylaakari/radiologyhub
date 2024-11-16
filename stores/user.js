e51fa6f02eab9e7c5cf4dd8051cb5dde09183c84
Lev Vorobev <thylaakari@yandex.ru>
2024-11-16 19:47:49 +0500

    pinia@0.6.0 test cause issue demi2

--- stores/user.js
+++ stores/user.js
@@ -6,6 +6,12 @@ export const useUserStore = defineStore('userStore', {
     setUser(user) {
       this.user = user
     },
+    async fetchUser(client) {
+      const {
+        data: { user },
+      } = await client.auth.getUser()
+      this.setUser(user)
+    },
   },
   getters: {
     getUser: (state) => state.user,

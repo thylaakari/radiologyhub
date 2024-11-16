bb20d19a1c14e2ad964e39e2ac8597980155bb6f
Lev Vorobev <thylaakari@yandex.ru>
2024-11-16 21:44:04 +0500

    delete store

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,12 +27,7 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
-  modules: [
-    '@nuxt/content',
-    '@nuxtjs/supabase',
-    '@vee-validate/nuxt',
-    '@pinia/nuxt',
-  ],
+  modules: ['@nuxt/content', '@nuxtjs/supabase', '@vee-validate/nuxt'],
   content: {
     markdown: {
       anchorLinks: false,
@@ -42,8 +37,5 @@ export default defineNuxtConfig({
     key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBrY3NmcnVodm1zYnB6Ym9iZW9nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE1MDc4MjcsImV4cCI6MjA0NzA4MzgyN30.PR8PF7UlZ9zno5em41FDHKKWDuIKBSZ6OXPN4NtPmyQ',
     url: 'https://pkcsfruhvmsbpzbobeog.supabase.co',
     redirect: false,
-  },
-  build: {
-    transpile: ['pinia'],
   },
 })
